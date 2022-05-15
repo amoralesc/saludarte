dde1e60c38003be4971859024294a8ca14c8c611
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-14 22:43:22 -0500

    Customize home template

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -165,3 +165,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the first name for the user."""
+        return self.first_name
