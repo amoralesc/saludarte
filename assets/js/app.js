ee5d41228a425495c29d0df67379def076fefbee
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-16 04:10:07 -0500

    Add localization to datatables

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
 // require("jquery");
 // require("datatables.net-dt")();
 
-var $  = require( 'jquery' );
-var dt = require( 'datatables.net' )( window, $ );
+var $ = require("jquery");
+var dt = require("datatables.net")(window, $);
 
 $(document).ready(function () {
-  $("#datatable").DataTable();
+  $("#datatable").DataTable({
+    language: {
+      url: '../static/datatables.es-ES.json'
+    }
+  });
 });
