39ec59ee6b0fc661bd6089e8db2d933b92b852d6
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-16 04:11:45 -0500

    Add users index view datatable, add custom 403

--- src/users/views.py
+++ src/users/views.py
@@ -1,10 +1,37 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.contrib.auth.mixins import (
+    #    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 
 User = get_user_model()
 
 
-class UsersIndexView(ListView):
+class StaffMemberRequiredMixin(UserPassesTestMixin):
+    """Verify that the current user is a staff member."""
+
+    raise_exception = False
+    login_url = reverse_lazy("login")
+
+    # Only authenticated staff users can access this view
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.is_staff
+
+
+class UsersIndexView(StaffMemberRequiredMixin, ListView):
+    """
+    It shows a list of all the users in the database.
+    Offers actions to edit the users:
+    - Create a new user
+    - View an existing user
+    - Edit an existing user
+    - Delete an existing user
+    """
+
     template_name = "users/index.html"
     context_object_name = "users"
 
