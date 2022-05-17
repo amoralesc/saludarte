43767d2a9cc874cf26dc509bb4ebc02b046f0a1a
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-17 12:34:26 -0500

    Add extra css property to input with icon

--- src/residents/urls.py
+++ src/residents/urls.py
@@ -6,4 +6,6 @@ app_name = "residents"
 urlpatterns = [
     # name: residents:index, path: /residentes/
     path("", views.ResidentsIndexView.as_view(), name="index"),
+    # name: residentes:new, path: /residentes/nuevo/
+    path("nuevo/", views.NewResidentView.as_view(), name="new"),
 ]
--- src/residents/views.py
+++ src/residents/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from .models import Resident
 
@@ -15,6 +16,29 @@ class ResidentsIndexView(LoginRequiredMixin, ListView):
     - Delete an existing resident
     """
 
-    template_name = "residents/index.html"
     model = Resident
     context_object_name = "residents"
+    template_name = "residents/index.html"
+
+
+class NewResidentView(LoginRequiredMixin, CreateView):
+    """
+    It shows a form to create a new resident.
+    """
+
+    model = Resident
+    template_name = "residents/new_resident_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy("residents:index")
+
+    fields = [
+        "first_name",
+        "last_name",
+        "email",
+        "identification_type",
+        "identification_number",
+        "gender",
+        "is_staff",
+        "site",
+    ]
