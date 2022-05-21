9d55ca2639384fbbf44b5cf473e3864107fc3ace
amoralesc <a.moralesc@javeriana.edu.co>
2022-05-20 19:51:44 -0500

    Create base post medication, refactor new medication, create edit medication view

--- src/medications/urls.py
+++ src/medications/urls.py
@@ -7,8 +7,10 @@ app_name = "medications"
 urlpatterns = [
     # name: medications:index, path: /medicamentos/
     path("", views.MedicationsIndexView.as_view(), name="index"),
-    # name: medications:create, path: /medicamentos/crear/
-    path("crear/", views.create_medication, name="create"),
+    # name: medications:new, path: /medicamentos/nuevo/
+    path("nuevo/", views.create_medication, name="new"),
+    # name: medications:edit, path: /medicamentos/<pk>/editar/
+    path("<int:pk>/editar/", views.edit_medication, name="edit"),
     # name: medications:delete, path: /medicamentos/<pk>/eliminar/
     path(
         "<int:pk>/eliminar/",
--- src/medications/views.py
+++ src/medications/views.py
@@ -31,7 +31,11 @@ class MedicationsIndexView(LoginRequiredMixin, ListView):
 
 def create_medication(request):
     """
-    It creates a new medication.
+    It shows a form to create a new medication.
+    It does so by rendering two forms:
+    - A medication form to create the medication model.
+    - Multiple presentation forms to create the presentations
+    of the medications, using the modelformset_factory.
     """
 
     MedicationForm = modelform_factory(
@@ -44,7 +48,7 @@ def create_medication(request):
         can_delete=True,
     )
 
-    if request.method == "POST":
+    if request.method == "POST":  # If the forms were submitted
         medication_form = MedicationForm(request.POST)
         presentation_formset = PresentationFormSet(request.POST, request.FILES)
 
@@ -63,7 +67,8 @@ def create_medication(request):
                 request, "The medication has been created successfully."
             )
             return redirect("medications:index")
-    else:
+    else:  # First GET request
+        # Create empty forms
         medication_form = MedicationForm()
         presentation_formset = PresentationFormSet(
             queryset=Presentation.objects.none()
@@ -71,7 +76,67 @@ def create_medication(request):
 
     return render(
         request,
-        "medications/create_medication.html",
+        "medications/new_medication.html",
+        # Additional context are the forms
+        {
+            "medication_form": medication_form,
+            "presentation_formset": presentation_formset,
+        },
+    )
+
+
+def edit_medication(request, pk):
+    """
+    It shows a form to edit an existing medication.
+    It does so by rendering two forms:
+    - A medication form to edit the medication model.
+    - Multiple presentation forms to edit the presentations
+    of the medications, using the modelformset_factory.
+    """
+
+    medication = get_object_or_404(Medication, pk=pk)
+    MedicationForm = modelform_factory(
+        Medication,
+        fields=("name", "description"),
+    )
+    PresentationFormSet = modelformset_factory(
+        Presentation,
+        fields=("type", "measure_unit", "measure"),
+        can_delete=True,
+    )
+
+    if request.method == "POST":  # If the forms were submitted
+        medication_form = MedicationForm(request.POST, instance=medication)
+        presentation_formset = PresentationFormSet(
+            request.POST,
+            request.FILES,
+            # queryset=Presentation.objects.filter(medication=medication),
+        )
+
+        if medication_form.is_valid() and presentation_formset.is_valid():
+            medication = medication_form.save()
+            presentations = presentation_formset.save(commit=False)
+
+            # Set the medication to the presentations
+            # for presentation in presentations:
+            #    presentation.medication = medication
+            #    presentation.save()
+
+            messages.success(
+                request, "The medication has been edited successfully."
+            )
+            return redirect("medications:index")
+    else:  # First GET request
+        # Create forms with the data of the medication
+        medication_form = MedicationForm(instance=medication)
+        presentation_formset = PresentationFormSet(
+            queryset=Presentation.objects.filter(medication=medication)
+        )
+
+    return render(
+        request,
+        "medications/edit_medication.html",
+        # Additional context are the forms
         {
             "medication_form": medication_form,
             "presentation_formset": presentation_formset,
@@ -85,6 +150,9 @@ class DeleteMedicationView(LoginRequiredMixin, DeleteView):
     """
 
     model = Medication
+    # Only allow POST requests (DeleteView deletes objects using POST, crazy right?)
+    # TODO: Maybe create a custom view that only allows DELETE requests?
+    http_method_names = ["post"]
 
     def get_success_url(self):
         messages.success(
